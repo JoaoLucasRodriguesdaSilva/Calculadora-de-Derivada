3e902dcae70aff84739a2167de531176d209068f
Mourvim <108086840+Mourvim@users.noreply.github.com>
2023-10-30 19:46:48 -0300

    Delete main directory

--- main/go.mod
+++
@@ -1,7 +0,0 @@
-module home/main
-
-go 1.19
-
-replace home/derivada => ../derivada
-
-require home/derivada v0.0.0-00010101000000-000000000000
--- main/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-import "home/derivada"
-
-func main() {
-  fmt.Println("Hello World")
-  derivada.CalcDer(1.0)
-}
